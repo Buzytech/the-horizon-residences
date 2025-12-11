b1cd07552af3fc844d6afe87b4367fb38e936086
TechnologyBuzytech <technology@buzytech.com>
2025-12-11 16:55:24 +0530

    form page added

---
+++ src/app/strategic-partner/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './strategic-partner.module.css';
+
+const strategicPartnerPage = () => {
+  return (
+    <div className={styles.formArea}>
+        <div></div>
+    </div>
+  )
+}
+
+export default strategicPartnerPage
