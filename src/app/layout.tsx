3ca078e3d8bc903c9d29a5e60fe4226060ca75aa
TiwariVish <vishal97tiwari@gmail.com>
2025-12-24 12:26:57 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "The Horizon Residences – Luxury 4 & 5 BHK in Indirapuram",
-  description: "The Horizon Residences offers 4 & 5 BHK luxury apartments / flats in Indirapuram. Explore ultra luxury residential property by Harmony Infra in Delhi NCR.",
+  description:
+    "The Horizon Residences offers 4 & 5 BHK luxury apartments / flats in Indirapuram. Explore ultra luxury residential property by Harmony Infra in Delhi NCR.",
 };
 
 export default function RootLayout({
@@ -15,23 +15,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <head>
-        <meta name="google-site-verification" content="wsgUBFKHcbxFw2vFuYCAz90F2yo-vMyNjcg-CYpAim4" />
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9N4VK688HF"
+      <head>
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="wsgUBFKHcbxFw2vFuYCAz90F2yo-vMyNjcg-CYpAim4"
         />
-        <Script id="google-analytics">
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-9N4VK688HF');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-ND75NMNS');
           `}
         </Script>
       </head>
 
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ND75NMNS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
