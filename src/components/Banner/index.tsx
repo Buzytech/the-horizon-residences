1036ae1d24c29b1d102d4f8c8dd4374dd7f2230c
TiwariVish <vishal97tiwari@gmail.com>
2025-12-27 16:44:15 +0530

    link add

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -8,35 +8,45 @@ const Banner = () => {
       <section id="overview" className={styles.mainBannerSection}>
         <div className={`horizonContainer ${styles.bannerContainer}`}>
           <div className={styles.imageWrapperHorizon}>
-              <Image
-                src="/assets/images/banner/webBannerHorizon.webp"
-                alt="banner"
-                className={styles.bannerImage}
-                fill
-              />
-              <Image
-                  src="/assets/images/banner/mobBannerHorizon.webp"
-                  alt="banner"
-                  className={styles.mobBannerImage}
-                  fill
-                />
+            <Image
+              src="/assets/images/banner/webBannerHorizon.webp"
+              alt="banner"
+              className={styles.bannerImage}
+              fill
+            />
+            <Image
+              src="/assets/images/banner/mobBannerHorizon.webp"
+              alt="banner"
+              className={styles.mobBannerImage}
+              fill
+            />
           </div>
         </div>
       </section>
 
       <div className={styles.fixedIcons}>
-        <div className={styles.iconItem}>
+        <a href="tel:+917065212121" className={styles.iconItem}>
           <img src="/assets/images/logo/phonicon.svg" alt="Call" />
           <span className={styles.iconLabel}>CALL NOW</span>
-        </div>
-        <div className={styles.iconItem}>
+        </a>
+
+        <a
+          href="mailto:your-email@example.com?subject=Enquiry"
+          className={styles.iconItem}
+        >
           <img src="/assets/images/logo/messageicon.svg" alt="Chat" />
           <span className={styles.iconLabel}>ENQUIRE</span>
-        </div>
-        <div className={styles.iconItem}>
+        </a>
+
+        <a
+          href="https://wa.me/917065212121"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.iconItem}
+        >
           <img src="/assets/images/logo/whatsapicon.svg" alt="WhatsApp" />
           <span className={styles.iconLabel}>WHATSAPP</span>
-        </div>
+        </a>
       </div>
     </>
   );
